309650f5e059360de2eabfc3793bc92e42f29433
hbahramian <habahram@iu.edu>
2024-03-17 23:42:10 -0700

    .

---
+++ src/main/java/edu/iu/c322/midterm/controllers/QuizController.java
@@ -0,0 +1,16 @@
+package edu.iu.c322.midterm.controllers;
+
+import edu.iu.c322.midterm.model.Question;
+import edu.iu.c322.midterm.model.Quiz;
+import edu.iu.c322.midterm.repository.FileRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/quizzes")
+public class QuizController {
+
+}
---
+++ src/test/java/edu/iu/c322/midterm/MidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.iu.c322.midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
